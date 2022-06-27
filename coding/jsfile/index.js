aca25d13b4387161c29d74e84772ee53b7cf9781
sarfaraz-deshwali <102010896+sarfaraz-deshwali@users.noreply.github.com>
2022-06-27 18:18:35 +0530

    Add files via upload

---
+++ coding/jsfile/index.js
@@ -0,0 +1,74 @@
+function ans(){
+	document.getElementById("demo").innerHTML = "nothing bro and you "
+}
+
+function sas(){
+
+document.getElementById("pintu").innerHTML = "correctly."
+}
+
+function asa(){
+	document.getElementById("raja").innerHTML = "stephan buddy"
+}
+function sds(){
+	document.getElementById('myImage').src='https://images.hdqwalls.com/wallpapers/doctor-strange-multiverse-of-madness-lightning-0a.jpg'
+}
+
+function sdss(){
+	document.getElementById('myImage').src='https://images.hdqwalls.com/wallpapers/sinister-strange-th.jpg'
+}
+function tio(){
+
+	document.getElementById("cemo").style.display='block'
+}
+function too(){
+
+	document.getElementById("emo"). style. fontFamily = "sans-serif"
+}
+function to(){
+
+	document.getElementById("emo").style. fontFamily =  "Impact"
+}
+function t(){
+
+	document.getElementById("emo").style.fontFamily="nothing"
+}
+
+
+function test(){
+
+
+let x;
+x = 5;
+document.getElementById("mnm").innerHTML=x;
+}
+
+function se(){
+	var x = 34
+var y = 56
+var z = x+y 
+document.getElementById("mun").innerHTML=z
+}
+function qe(){
+	var x = 34
+var y = 56
+var z = x/y 
+document.getElementById("mon").innerHTML=z
+}
+function re(){
+	var x = 34
+var y = 56
+var z = x*y 
+document.getElementById("myn").innerHTML=z
+}
+function te(){
+	var x = 34
+var y = 56
+var z = x%y 
+document.getElementById("mpn").innerHTML=z
+}
+function d(){
+	var x = 41
+document.getElementById("mn").innerHTML= x +500059
+}
+
